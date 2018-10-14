4e9cc72e8bc2d4b4ba7ac4247ae4867af695fe62
CTHobbit <ben.schenkman@gmail.com>
2018-10-13 21:01:31 -0400

    Successful query of specific program and student data plus histogram

--- pythonviz.py
+++ pythonviz.py
@@ -1,5 +1,9 @@
 import sqlite3 as lite
 import sys
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib import colors
+from matplotlib.ticker import PercentFormatter
 
 #clear connection variable
 con = None
@@ -9,25 +13,38 @@ try:
     con = lite.connect('sampledb.sqlite')
     
     cur = con.cursor()    
-    cur.execute('SELECT SQLITE_VERSION()')
+    #cur.execute('SELECT SQLITE_VERSION()')
 
-    data = cur.fetchone()
+    #data = cur.fetchone()
     
-    print ("SQLite version: %s" % data)
+    #print ("SQLite version: %s" % data)
 
-    cur.execute('SELECT * from Assignments')
+    #PEO1 Scores
+    cur.execute('SELECT Score from Assignments where PEO = "PEO1"')
+    PEO1 = cur.fetchall()
 
-    rows = cur.fetchall()
+    #SO1 Scores
+    cur.execute('SELECT Score from Assignments where SO = "SO1"')
+    SO1 = cur.fetchall()
 
-    for row in rows:
-        print (rows)
+    avgso1 = np.mean(SO1)
+
+    #for row in rows:
+    #print (rows)
         
 except lite.Error as e :
     
     print ("Error %s:" % e.args[0])
     sys.exit(1)
-    
+
 finally:
     
     if con:
         con.close()
+
+print(avgso1)
+
+a = np.array(PEO1) 
+plt.hist(a) 
+plt.title("Distribution of Scores for PEO1")
+plt.show()
