71ad1924673d46d98f513bbda15fd9e2acf03058
CTHobbit <ben.schenkman@gmail.com>
2018-10-21 21:02:11 -0400

    Added term and year to DB, separated out sub plot to allow for multiple plots in windows, added scatter plot

--- pythonviz.py
+++ pythonviz.py
@@ -31,6 +31,9 @@ try:
     cur.execute('SELECT Score from Assignments where SO = "SO1"')
     SO1 = cur.fetchall()
 
+    #PEO1 Scores by 
+    cur.execute('Select * from Assignments where PEO = "PEO1"')
+    PEO1Scores = np.array(cur.fetchall())
     
         
 except lite.Error as e :
@@ -47,18 +50,31 @@ finally:
 #avgso1 = round(np.mean(SO1),2)
 #print(avgso1)
 
-#Combined SO1 and PEO1 Histogram
+#Data for combined SO1 and PEO1 Histogram
 a = np.array(PEO1) 
 b = np.array(SO1)
 bins = (50,60,70,80,90,100)
 
+#Data for bar graph showing PEO1 by Year
+PEO1Scores = PEO1Scores[PEO1Scores[:,7].argsort()]
+x = PEO1Scores[:,7]
+y = PEO1Scores[:,5]
+
 #setting style to seaborn to make it look better: https://seaborn.pydata.org/tutorial/aesthetics.html
 sns.set()
 
+plt.figure(1)
+plt.subplot(211)
 plt.hist([a, b], bins, label=['PEO1', 'SO1'])
 plt.title("Distribution of Scores for PEO1 and SO1")
 plt.legend(loc='upper right')
+
+#Scatter plot of scores by year
+plt.subplot(212)
+plt.scatter(x,y)
+
 plt.show()
+
 
 #Combined interactive histograms with plotly, proof of concept, creates a plot
 #online using an API and is stored at https://plot.ly/~bschenkman/0
