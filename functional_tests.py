cb30591ea7802e35e9b0bc479b0bc3f8e9939a35
Tom Glenn <tommyg78@gmail.com>
2018-11-16 17:26:34 -0600

    First TDD tests

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,27 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Faculty Member goes to the page to enter assignment data
+        self.browser.get('http://localhost:8000')
+
+        # He/She  notices the page title and header mention entering assignment data. 
+        self.assertIn('Enter Assignment Data', self.browser.title)
+        self.fail('Finish the test!')
+
+
+        # The Faculty member is invited to enter assignment data straight away
+
+if __name__ == '__main__':  
+    unittest.main(warnings='ignore')        
+
+
+
